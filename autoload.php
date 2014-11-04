b10763585271da2f238786561f9b2ff1fedd85e1
David Molineus <mail@netzmacht.de>
2014-11-04 17:18:31 +0100

    only run autoloader if contao is installed

--- autoload.php
+++ autoload.php
@@ -2,6 +2,9 @@
 
 define('TL_ROOT', dirname(dirname(__DIR__)) . '/contao/core');
 
-require_once __DIR__ . '/src/Netzmacht/Contao/BuildTools/ClassLoader.php';
+// only load fake autoloader if we are in the contao context.
+if (file_exists(TL_ROOT . '/system/initialize.php')) {
+    require_once __DIR__ . '/src/Netzmacht/Contao/BuildTools/ClassLoader.php';
 
-\Netzmacht\Contao\BuildTools\ClassLoader::scanAndRegister();
+    \Netzmacht\Contao\BuildTools\ClassLoader::scanAndRegister();
+}
